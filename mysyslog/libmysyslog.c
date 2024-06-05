efaceff7c9587c3418d4d63b2e31f51cf6559c6a
FuriousLoco <167618905+FuriousLoco@users.noreply.github.com>
2024-06-05 23:26:22 +0300

    Update libmysyslog.c

--- mysyslog/libmysyslog.c
+++ mysyslog/libmysyslog.c
@@ -1,33 +1,14 @@
+#include "mysyslog.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <syslog.h>
+#include <time.h>
 
-// Функция для записи в журнал
-void mysyslog(const char* msg, int level, int driver, int format, const char* path) {
-    // Определение уровня журналирования
-    switch (level) {
-        case DEBUG:
-            syslog(LOG_DEBUG, "%s", msg);
-            break;
-        case INFO:
-            syslog(LOG_INFO, "%s", msg);
-            break;
-        case WARN:
-            syslog(LOG_WARNING, "%s", msg);
-            break;
-        case ERROR:
-            syslog(LOG_ERR, "%s", msg);
-            break;
-        case CRITICAL:
-            syslog(LOG_CRIT, "%s", msg);
-            break;
-    }
-}
+int mysyslog(const char* msg, int level, int driver, int format, const char* path) {
+    // Пример базовой реализации
+    FILE* file = fopen(path, "a");
+    if (!file) return -1;
 
-int main() {
-    // Запись в журнал
-    mysyslog("This is a test message", INFO, 0, 0, "/var/log/syslog");
-
+    time_t now = time(NULL);
+    fprintf(file, "%ld %d %d %d %s\n", now, level, driver, format, msg);
+    fclose(file);
     return 0;
 }
